d10253bf43dded0b7fea4c2737eed5fcfb9fec31
Kelei Gong <kgong@ncsu.edu>
2015-10-17 19:43:43 -0400

    basic merge completed

--- hw3/main.c
+++ hw3/main.c
@@ -4,69 +4,89 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <sys/time.h>
 
 int numOfInt(FILE *inp);
 int cmpfunc(const void * a, const void * b);
-int readLeftMost(int *ptr);
-void setLeftMost(int *ptr);
+int readLeftMost(int *ptr, int buffer_size);
+void setLeftMost(int *ptr, int buffer_size);
+void basicMerge(FILE *inp);
+void multiMerge(FILE *inp);
 
 int main(int argc, char *argv[])
 {
-    FILE *inp = fopen( "input.bin", "r");
-    int len = numOfInt(inp);
-    printf("%d\n", len);
+    struct timeval start_time, end_time;
+    FILE *inp = fopen("input.bin", "r");
+    gettimeofday( &start_time, NULL );
+    basicMerge(inp);
+    gettimeofday( &end_time, NULL );
+    printf("Time: %lf\n", (end_time.tv_sec - start_time.tv_sec)+(end_time.tv_usec-start_time.tv_usec)/1000000.0);
+    return 0;
+}
+
+void basicMerge(FILE *inp)
+{
+    FILE *outfile = fopen( "sort.bin", "w");
+    int len = numOfInt(inp); // calculate number of int to sort
+
     int *input = malloc(1000 * sizeof(int));
     int *output = malloc(1000 * sizeof(int));
+
+    //----- Create runs -----
     int num_of_runs = len / 1000;
+    if( num_of_runs * 1000 < len) //If len can't be divided by 1000 equally, add an addition run
+    {
+        num_of_runs += 1;
+    }
     int i;
     FILE *fp;
     char *filename = malloc(13 * sizeof(char));
-//    char *output_filename = "input.bin.001";
-//    printf("%s\n", output_filename);
-//    for( i = 1; i <= num_of_runs; i++ )
-//    {
-//        fread(input, sizeof(int), 1000, inp);
-//        qsort(input, 1000, sizeof(int), cmpfunc);
-////        char *output_file = malloc(13 * sizeof(char));
-//        sprintf(filename, "input.bin.%03d", i);
-//        fp = fopen(filename, "w");
-//        fwrite(input, sizeof(int), 1000, fp);
-//        fclose(fp);
-////        free(filename);
-//    }
-    int num_to_buffer = 1000 / num_of_runs;
-    int *buffer = malloc(num_to_buffer * sizeof(int));
-    FILE *files[250];
+    for( i = 1; i <= num_of_runs - 1; i++ ) // The first num_of_runs-1 runs are sure to have 1000 int
+    {
+        fread(input, sizeof(int), 1000, inp);
+        qsort(input, 1000, sizeof(int), cmpfunc);
+        sprintf(filename, "input.bin.%03d", i);
+        fp = fopen(filename, "w");
+        fwrite(input, sizeof(int), 1000, fp);
+        fclose(fp);
+    }
+    fread(input, sizeof(int), (size_t) (len-1000*(num_of_runs - 1)), inp); // The last run may have less than 1000 int
+    printf("%d\n", (len-1000*(num_of_runs - 1)));
+    qsort(input, (size_t) (len-1000*(num_of_runs - 1)), sizeof(int), cmpfunc);
+    sprintf(filename, "input.bin.%03d", num_of_runs);
+    fp = fopen(filename, "w");
+    fwrite(input, sizeof(int), (size_t) (len-1000*(num_of_runs - 1)), fp);
+    fclose(fp);
+
+    int num_to_buffer = 1000 / num_of_runs; // number of int to buffer for each run.
+
+    FILE *files[num_of_runs];
     for( i = 1; i <= num_of_runs; i++ )
     {
         sprintf(filename, "input.bin.%03d", i);
         files[i-1] = fopen(filename, "r");
     }
 
-    int *ptr[250];
+    int *ptr[num_of_runs];
     for( i = 0; i < num_of_runs; i++ )
     {
-//        fseek(files[i-1], (i-1)*4* sizeof(int), SEEK_SET);
-        fread(input + i * 4, sizeof(int), 4, files[i]);
-        ptr[i] = input + i * 4;
-//        printf("%d\n",  *ptr[i]);
+        fread(input + i * num_to_buffer, sizeof(int), (size_t) num_to_buffer, files[i]);
+        ptr[i] = input + i * num_to_buffer;
     }
-    printf("\n");
     int tmp;
-    int min = input[0];
+    int min;
     int min_index = 0;
     int w = 0;
-    int out = 0;
-    while(w < 1000)
+    int out_index = 0;
+    int empty_file = 0;
+    while(w < len)
     {
-        min = readLeftMost(ptr[0]);
+        min = INT_MAX;
         min_index = 0;
-//        printf("%d\n", min);
         for( i = 0; i < num_of_runs; i++)
         {
-            tmp = readLeftMost(ptr[i]);
-//            printf("%d\n", tmp);
-
+            tmp = readLeftMost(ptr[i], num_to_buffer);
             if( tmp != -1)
             {
                 if( tmp < min)
@@ -77,45 +97,50 @@ int main(int argc, char *argv[])
             }
             else
             {
-                printf("!!!!\n");
-                fread(ptr[i], sizeof(int), 4, files[i]);
-                tmp = readLeftMost(ptr[i]);
-                if( tmp < min)
+                if(!feof(files[i]))
                 {
-                    min = tmp;
-                    min_index = i;
+                    fread(ptr[i], sizeof(int), (size_t) num_to_buffer, files[i]);
+                    tmp = readLeftMost(ptr[i], num_to_buffer);
+//                    if(tmp == -1)
+//                    {
+//                        empty_file++;
+//                        files[i] = NULL;
+//                    }
+                    if (tmp != -1 && tmp < min) {
+                        min = tmp;
+                        min_index = i;
+                    }
                 }
-            }
-        }
-        setLeftMost(ptr[min_index]);
-        if(readLeftMost(ptr[0]) == -1)
-        {
-            fread(ptr[i], sizeof(int), 4, files[0]);
-        }
-        output[out] = min;
-        out++;
-        if( out >= 1000)
-        {
-            out = 0;
-            for(i = 0; i < 1000; i++)
-            {
-                printf("%d,", output[i]);
-            }
-        }
 
-//        printf("%d, %d\n", min, min_index);
-//        break;
+            }
+        }
+        setLeftMost(ptr[min_index], num_to_buffer); // When find the smallest int, put it into output buffer
+        output[out_index] = min;
+        out_index++;
+        if( out_index >= 1000) // When the output buffer is full, reset the out_index and append to output file
+        {
+            out_index = 0;
+            fwrite(output, sizeof(int), 1000, outfile);
+        }
         w++;
     }
-
-
-    for(i = 0; i < num_of_runs; i++)
+//    printf("%d empty\n", empty_file);
+    if(out_index != 0)
     {
-//        printf("%d,", *ptr[i]);
+        fwrite(output, sizeof(int), (size_t) out_index, outfile);
     }
-    return 0;
+    fclose(outfile);
 }
 
+void merge()
+{
+
+}
+
+void multiMerge(FILE *inp)
+{
+
+}
 
 int numOfInt(FILE *inp){
     fseek(inp, 0, SEEK_END);
@@ -129,39 +154,30 @@ int cmpfunc(const void * a, const void * b)
     return ( *(int*)a - *(int*)b );
 }
 
-int readLeftMost(int *ptr)
+int readLeftMost(int *ptr, int buffer_size)
 {
     int i;
     int res;
-    for( i = 0; i < 4; i++)
+    for( i = 0; i < buffer_size; i++)
     {
         if( *(ptr + i) != -1)
         {
             res = *(ptr + i);
-//            printf("%d\n", res);
-//            *(ptr + i) = -1;
             return res;
         }
     }
     return -1;
 }
 
-void setLeftMost(int *ptr)
+void setLeftMost(int *ptr, int buffer_size)
 {
     int i;
-    for(i = 0; i < 4; i++)
+    for(i = 0; i < buffer_size; i++)
     {
         if( *(ptr + i) != -1)
         {
-//            printf("%d\n", *(ptr + i));
             *(ptr + i) = -1;
-//            printf("%d\n", *(ptr + i));
             break;
         }
     }
-}
-
-void readIntoMem(FILE *inp, int* input)
-{
-
 }
