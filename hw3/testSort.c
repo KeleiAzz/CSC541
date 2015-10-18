b77cbdd565c9cac54db17137b68f83b90b29d0d1
Kelei Gong <kgong@ncsu.edu>
2015-10-18 16:10:47 -0400

    Add a test program

---
+++ hw3/testSort.c
@@ -0,0 +1,69 @@
+//
+// Created by Kelei Gong on 10/16/15.
+//
+
+//#include "testSort.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+int numOfInt(FILE *inp);
+
+int main()
+{
+    FILE *outfile = fopen( "sort_re.bin", "r");
+    FILE *example = fopen("sort_example.bin", "r");
+    int len = numOfInt(outfile);
+    printf("%d\n", len);
+    int *allall = malloc(len * sizeof(int));
+    int *sorted = malloc(len * sizeof(int));
+
+    fread(allall, sizeof(int), (size_t) len, outfile);
+    fread(sorted, sizeof(int), (size_t) len, example);
+    char *filename = malloc(16 * sizeof(char));
+    int i, j, num = 0;
+    for(i = 0; i < len-1; i++)
+    {
+        if(allall[i] != sorted[i])
+            {
+            printf("Wrong!!! %d %d \n", allall[i], sorted[i]);
+            }
+        if(allall[i+1] < allall[i])
+        {
+                printf("Wrong!!! %d %d \n", allall[i], allall[i+1]);
+        }
+    }
+//    for(j = 1; j <= 168; j++)
+//    {
+//        sprintf(filename, "input.bin.re.%03d", j);
+//        FILE *re = fopen(filename, "r");
+//        len = numOfInt(re);
+//        num += len;
+//        int *all = malloc(len * sizeof(int));
+////        int *sorted = malloc(len * sizeof(int));
+//        printf("%d\n", len);
+//        fread(all, sizeof(int), (size_t) len, outfile);
+//        for(i = 0; i < len-1; i++)
+//        {
+////            if(all[i] != sorted[i])
+////            {
+//////            printf("Wrong!!! %d %d \n", all[i], sorted[i]);
+////            }
+//            if(all[i+1] < all[i])
+//            {
+////                printf("Wrong!!! %d %d \n", all[i], all[i+1]);
+//            }
+//        }
+//        printf("total %d\n", num);
+//    }
+
+    return 0;
+}
+
+
+int numOfInt(FILE *inp){
+    fseek(inp, 0, SEEK_END);
+    unsigned long len = (unsigned long)ftell(inp);
+    rewind(inp);
+    return (int) (len/4);
+};
